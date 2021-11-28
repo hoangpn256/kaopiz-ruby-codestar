0d1c041bc9e13bd78e2c0cad0592f5e609907fef
Hoshimachi <a2giacat100@gmail.com>
2021-11-28 15:18:43 +0700

    sua loi

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, uniqueness: true
     after_create :print
-    after_destroy :delete_history
+    after_destroy :deleteHistory
 
     private
 
@@ -10,7 +10,7 @@ class User < ApplicationRecord
         p "User is created in #{timestamps}"
     end
 
-    def delete_history
+    def deleteHistory
         UserDeleteHistory.create(email: self.email, name: self.name, dob: self.dob, address: self.address)
     end
 end
